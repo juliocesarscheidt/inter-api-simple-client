e622c69cb1960d85199d47459acc554642dc6b23
juliocesarscheidt <julio.scheidt96@gmail.com>
2025-02-16 19:56:11 -0300

    Separate project

--- api/ui/src/filters/filters.js
+++
@@ -1,5 +0,0 @@
-const currency = value => `R$ ${parseFloat(value).toFixed(2).toLocaleString()}`
-
-export {
-  currency
-}
--- api/ui/src/utils/translations/index.js
+++
@@ -1,9 +0,0 @@
-import enus from './en-us';
-import ptbr from './pt-br';
-
-const locales = {
-  'en-us': enus,
-  'pt-br': ptbr,
-};
-
-export default locales;
