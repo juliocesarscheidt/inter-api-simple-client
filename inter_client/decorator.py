fd411a300d0fe428f93f43f3a50bf3c01d539e3a
juliocesarscheidt <julio.scheidt96@gmail.com>
2025-02-18 22:46:20 -0300

    Improve decorator

--- inter_client/decorator.py
+++ inter_client/decorator.py
@@ -5,12 +5,16 @@ def request_retry(func):
     def wrapper(*args, max_retries: int = 5, **kwargs):
         attemps, wait_time = 0, 2
         while True:
-            # print(f"Requesting {func.__name__} with args: {args} and kwargs: {kwargs}")
             try:
                 response = func(*args, **kwargs)
                 return response
             except Exception as error:
                 if attemps >= max_retries:
+                    raise error
+                # transient errors: 408, 429, 502, 503, 504
+                # 408: Request Timeout | 429: Too Many Requests | 502: Bad Gateway | 503: Service Unavailable | 504: Gateway Timeout
+                if error.response is not None and error.response.status_code is not None \
+                and error.response.status_code not in [408, 429, 502, 503, 504]:
                     raise error
                 attemps += 1
                 print(f"Request error. Retrying in {wait_time} seconds")
